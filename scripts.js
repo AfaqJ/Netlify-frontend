367d8705a39c59436a1a065c257d1c1c2278ebf2
Afaq <92641524+AfaqJ@users.noreply.github.com>
2024-01-26 02:54:39 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,22 +1,24 @@
+
+
 function predict() {
-    var inputText = document.getElementById('inputText').value;
+  var inputText = document.getElementById('inputText').value;
 
-    if (inputText.trim() === "") {
-        alert("Please enter text before predicting.");
-        return;
-    }
+  if (inputText.trim() === "") {
+    alert("Please enter text before predicting.");
+    return;
+  }
 
-    fetch('https://huggingface.co/spaces/AfaqJamshaid/textclassification/predict', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'accept': 'application/json'
-        },
-        body: JSON.stringify({ text: inputText })
-    })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('outputLabel').innerText = 'Predicted Label: ' + data.predicted_label;
-    })
-    .catch(error => console.error('Error:', error));
+  fetch('/.netlify/functions/proxy', {  // Point to your serverless function
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'accept': 'application/json'
+    },
+    body: JSON.stringify({ text: inputText })
+  })
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('outputLabel').innerText = 'Predicted Label: ' + data.predicted_label;
+  })
+  .catch(error => console.error('Error:', error));
 }
